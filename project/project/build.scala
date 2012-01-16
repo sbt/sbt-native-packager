68cebccd324edc29db82bf495930a98205629d01
Josh Suereth <joshua.suereth@gmail.com>
2012-01-16 15:01:07 -0500

    added gpg plugin.

--- project/project/build.scala
+++ project/project/build.scala
@@ -1,8 +1,7 @@
 import sbt._
 object PluginDef extends Build {
   override def projects = Seq(root)
-  lazy val root = Project("plugins", file(".")) dependsOn(ghpages)
-  // move back to josh's repo after pull requests for git branch key
-  // support goes through
+  lazy val root = Project("plugins", file(".")) dependsOn(ghpages, gpg)
   lazy val ghpages = uri("git://github.com/jsuereth/xsbt-ghpages-plugin.git")
+  lazy val gpg = uri("git://github.com/sbt/xsbt-gpg-plugin.git#0.4")
 }
