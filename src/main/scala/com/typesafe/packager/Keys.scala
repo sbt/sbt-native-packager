55031d76b63a9dd81f48e203d659a23962d88318
Josh Suereth <joshua.suereth@gmail.com>
2012-07-18 10:41:20 -0400

    Keys were missing universal keys.  NOW FIXED

--- src/main/scala/com/typesafe/packager/Keys.scala
+++ src/main/scala/com/typesafe/packager/Keys.scala
@@ -1,3 +1,7 @@
 package com.typesafe.packager
 
-object Keys extends linux.Keys with debian.DebianKeys with rpm.RpmKeys with windows.WindowsKeys {}
+object Keys extends linux.Keys 
+  with debian.DebianKeys 
+  with rpm.RpmKeys 
+  with windows.WindowsKeys
+  with universal.UniversalKeys {}
