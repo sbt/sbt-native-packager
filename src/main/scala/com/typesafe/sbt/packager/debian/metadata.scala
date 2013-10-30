ed1b91a0855f6775d7092f207cfac01179eb43e4
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2013-10-30 19:57:46 +0100

    Debian scripts can now be filtered #35

--- src/main/scala/com/typesafe/sbt/packager/debian/metadata.scala
+++ src/main/scala/com/typesafe/sbt/packager/debian/metadata.scala
@@ -2,14 +2,12 @@ package com.typesafe.sbt
 package packager
 package debian
 
-
 case class PackageInfo(
   name: String,
   version: String,
   maintainer: String,
   summary: String,
-  description: String
-)
+  description: String)
 
 /** Represents package meta used by debian when constructing packages. */
 case class PackageMetaData(
@@ -18,8 +16,7 @@ case class PackageMetaData(
   architecture: String = "all",
   section: String = "java",
   depends: Seq[String] = Seq.empty,
-  recommends: Seq[String] = Seq.empty
-) {
+  recommends: Seq[String] = Seq.empty) {
   def makeContent(installSizeEstimate: Long = 0L): String = {
     // TODO: Pretty print with line wrap.
     val sb = new StringBuilder
@@ -29,12 +26,33 @@ case class PackageMetaData(
     sb append ("Priority: %s\n" format priority)
     sb append ("Architecture: %s\n" format architecture)
     sb append ("Installed-Size: %d\n" format installSizeEstimate)
-    if(!depends.isEmpty)
+    if (!depends.isEmpty)
       sb append ("Depends: %s\n" format (depends mkString ", "))
-    if(!recommends.isEmpty)
+    if (!recommends.isEmpty)
       sb append ("Recommends: %s\n" format (recommends mkString ", "))
     sb append ("Maintainer: %s\n" format info.maintainer)
     sb append ("Description: %s\n %s\n" format (info.summary, info.description))
     sb toString
   }
+}
+
+/**
+ * This replacements are use for the debian maintainer scripts:
+ * preinst, postinst, prerm, postrm
+ */
+case class DebianControlScriptReplacements(
+  author: String,
+  descr: String,
+  name: String,
+  version: String) {
+
+  /**
+   * Generates the replacement sequence for the debian
+   * maintainer scripts
+   */
+  def makeReplacements(): Seq[(String, String)] = Seq(
+    "author" -> author,
+    "descr" -> descr,
+    "name" -> name,
+    "version" -> version)
 }
