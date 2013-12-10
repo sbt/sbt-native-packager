4db2ef6da0293529b8d2762e34b4c82afdf79f01
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2013-12-10 22:50:18 +0100

    Setting chdir to application directory. So logs get placed in the right place (in play applications). Not relative to the /bin folder.

--- src/main/scala/com/typesafe/sbt/packager/archetypes/JavaServerApplication.scala
+++ src/main/scala/com/typesafe/sbt/packager/archetypes/JavaServerApplication.scala
@@ -6,7 +6,7 @@ import Keys._
 import sbt._
 import sbt.Keys.{ target, mainClass, normalizedName }
 import SbtNativePackager._
-import com.typesafe.sbt.packager.linux.{LinuxFileMetaData, LinuxPackageMapping}
+import com.typesafe.sbt.packager.linux.{ LinuxFileMetaData, LinuxPackageMapping }
 
 /**
  * This class contains the default settings for creating and deploying an archetypical Java application.
@@ -27,46 +27,43 @@ object JavaServerAppPackaging {
     Seq(
       debianStartScriptReplacements <<= (
         maintainer in Debian, packageSummary in Debian, serverLoading in Debian, daemonUser in Debian, normalizedName,
-          sbt.Keys.version, defaultLinuxInstallLocation, mainClass in Compile, scriptClasspath)
+        sbt.Keys.version, defaultLinuxInstallLocation, mainClass in Compile, scriptClasspath)
         map { (author, descr, loader, daemonUser, name, version, installLocation, mainClass, cp) =>
-        // TODO name-version is copied from UniversalPlugin. This should be consolidated into a setting (install location...)
-        val appDir = installLocation + "/" + name
-        val chdir = appDir + "/bin"
-        val appClasspath = cp.map(appDir + "/lib/" + _).mkString(":")
+          // TODO name-version is copied from UniversalPlugin. This should be consolidated into a setting (install location...)
+          val appDir = installLocation + "/" + name
+          val appClasspath = cp.map(appDir + "/lib/" + _).mkString(":")
 
-        JavaAppStartScript.makeReplacements(
-          author = author,
-          description = descr,
-          execScript = name,
-          chdir = chdir,
-          appName = name,
-          appClasspath = appClasspath,
-          appMainClass = mainClass.get,
-          daemonUser = daemonUser
-        )
-      },
+          JavaAppStartScript.makeReplacements(
+            author = author,
+            description = descr,
+            execScript = name,
+            chdir = appDir,
+            appName = name,
+            appClasspath = appClasspath,
+            appMainClass = mainClass.get,
+            daemonUser = daemonUser)
+        },
       debianMakeStartScript <<= (debianStartScriptReplacements, normalizedName, target in Universal, serverLoading in Debian)
         map makeDebianStartScript,
       debianMakeEtcDefault <<= (normalizedName, target in Universal, serverLoading in Debian)
         map makeEtcDefaultScript,
-      linuxPackageMappings in Debian <++= (debianMakeEtcDefault, normalizedName) map {(conf, name) =>
+      linuxPackageMappings in Debian <++= (debianMakeEtcDefault, normalizedName) map { (conf, name) =>
         conf.map(c => LinuxPackageMapping(Seq(c -> s"/etc/default/$name")).withConfig()).toSeq
       },
       linuxPackageMappings in Debian <++= (debianMakeStartScript, normalizedName, serverLoading in Debian)
         map { (script, name, loader) =>
-        val (path, permissions) = loader match {
-          case Upstart => ("/etc/init/" + name + ".conf", "0644")
-          case SystemV => ("/etc/init.d/" + name, "0755")
-        }
+          val (path, permissions) = loader match {
+            case Upstart => ("/etc/init/" + name + ".conf", "0644")
+            case SystemV => ("/etc/init.d/" + name, "0755")
+          }
 
-        for {
-          s <- script.toSeq
-        } yield LinuxPackageMapping(Seq(s -> path)).withPerms(permissions)
-      },
+          for {
+            s <- script.toSeq
+          } yield LinuxPackageMapping(Seq(s -> path)).withPerms(permissions)
+        },
       // TODO - only make these if the upstart config exists...
       debianMakePrermScript <<= (normalizedName, target in Universal) map makeDebianPrermScript,
       debianMakePostinstScript <<= (normalizedName, target in Universal, serverLoading in Debian) map makeDebianPostinstScript)
-
 
   private def makeDebianStartScript(
     replacements: Seq[(String, String)], name: String, tmpDir: File, loader: ServerLoader): Option[File] =
@@ -78,14 +75,12 @@ object JavaServerAppPackaging {
       Some(script)
     }
 
-
   protected def makeDebianPrermScript(name: String, tmpDir: File): Option[File] = {
     val scriptBits = JavaAppStartScript.generatePrerm(name)
     val script = tmpDir / "tmp" / "bin" / "debian-prerm"
     IO.write(script, scriptBits)
     Some(script)
   }
-
 
   protected def makeDebianPostinstScript(name: String, tmpDir: File, loader: ServerLoader): Option[File] = {
     val scriptBits = JavaAppStartScript.generatePostinst(name, loader)
