8492a6b72ecdda7851a2482178d6146724770f2d
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2014-04-02 19:49:07 +0200

    Styling the docs a bit.
    ```bash
    sudo apt-get install python-pip
    sudo pip install sphinx_bootstrap_theme
    ```

--- src/sphinx/conf.py
+++ src/sphinx/conf.py
@@ -12,6 +12,7 @@
 # serve to show the default.
 
 import sys, os
+import sphinx_bootstrap_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -91,15 +92,20 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinxdoc'
+html_theme = 'bootstrap'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
+html_theme_options = {
+    'bootstrap_version': "3",
+    'bootswatch_theme': "yeti",
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
