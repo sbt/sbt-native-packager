5a91a0be011e5f332f2cb4928da579363cace2cd
Joe Kutner <jpkutner@gmail.com>
2014-08-19 09:07:26 -0500

    added replacement test classes for staging-custom-main class

---
+++ src/sbt-test/universal/staging-custom-main/src/main/scala/CustomMain.scala
@@ -0,0 +1,7 @@
+object CustomMainOne extends App {
+  override def main( args:Array[String] ):Unit = println("Hello world 1")
+}
+
+object CustomMainTwo extends App {
+  override def main( args:Array[String] ):Unit = println("Hello world 2")
+}
