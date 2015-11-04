2e2bc1255092b11432e731e4379b4ec151581d5d
Felix Satyaputra <felix.satyaputra@typesafe.com>
2015-11-04 12:35:45 +1100

    Trying to fix race condition in `jar/classpath-jar` test
    Ensure actor system is shutdown only after the sent message is received by the actor under test as expected in the test

--- src/sbt-test/jar/classpath-jar/src/main/scala/test/Test.scala
+++ src/sbt-test/jar/classpath-jar/src/main/scala/test/Test.scala
@@ -3,17 +3,27 @@ package test
 // use dependency library
 
 import akka.actor._
+import akka.pattern._
 import akka.routing.RoundRobinRouter
+import akka.util.Timeout
+import scala.concurrent.duration._
 
 class PrintActor extends Actor{
   def receive = {
-    case msg => println(msg)
+    case msg =>
+      println(msg)
+      context.sender ! "ok"
   }
 }
 
 object Test extends App {
+  implicit val timeout = Timeout(3 seconds)
+
   val system = ActorSystem("testSystem")
+  import system.dispatcher
+
   val router = system.actorOf(Props[PrintActor])
-  router ! "SUCCESS!!"
-  system.shutdown
+  (router ? "SUCCESS!!").foreach { _ =>
+    system.shutdown
+  }
 }
