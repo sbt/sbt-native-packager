a68c7c3a6f4e71a83debadb64688bf56563abb52
Maciej Biłas <maciej.bilas@gmail.com>
2016-07-12 19:38:02 +0200

    docker: tag -f is deprecated (#821) (#845)
    fixes #818

--- src/main/scala/com/typesafe/sbt/packager/docker/DockerPlugin.scala
+++ src/main/scala/com/typesafe/sbt/packager/docker/DockerPlugin.scala
@@ -294,7 +294,7 @@ object DockerPlugin extends AutoPlugin {
 
     if (latest) {
       val name = tag.substring(0, tag.lastIndexOf(":")) + ":latest"
-      val latestCmd = Seq("docker", "tag", "-f", tag, name)
+      val latestCmd = Seq("docker", "tag", tag, name)
       Process(latestCmd).! match {
         case 0 => log.info("Update Latest from image " + tag)
         case n => sys.error("Failed to run docker tag")
