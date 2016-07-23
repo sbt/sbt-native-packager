ef62eeb0fcc7b98d290a8d767ac635a82e9169cd
Mathias Kub <git@makubi.at>
2016-07-23 17:25:09 +0200

    Rename dockerTarget to dockerTag
    Using 'SettingKey' instead of 'TaskKey' for the 'dockerTag' setting.

--- src/main/scala/com/typesafe/sbt/packager/docker/Keys.scala
+++ src/main/scala/com/typesafe/sbt/packager/docker/Keys.scala
@@ -10,12 +10,12 @@ import sbt._
 trait DockerKeys {
   val dockerGenerateConfig = TaskKey[File]("docker-generate-config", "Generates configuration file for Docker.")
   val dockerPackageMappings = TaskKey[Seq[(File, String)]]("docker-package-mappings", "Generates location mappings for Docker build.")
-  val dockerTarget = TaskKey[String]("docker-target", "Defines target used when building and publishing Docker image")
 
   val dockerBaseImage = SettingKey[String]("dockerBaseImage", "Base image for Dockerfile.")
   val dockerExposedPorts = SettingKey[Seq[Int]]("dockerExposedPorts", "Ports exposed by Docker image")
   val dockerExposedVolumes = SettingKey[Seq[String]]("dockerExposedVolumes", "Volumes exposed by Docker image")
   val dockerRepository = SettingKey[Option[String]]("dockerRepository", "Repository for published Docker image")
+  val dockerTag = SettingKey[String]("dockerTag", "Docker tag for the built image")
   val dockerUpdateLatest = SettingKey[Boolean]("dockerUpdateLatest", "Set to update latest tag")
   val dockerEntrypoint = SettingKey[Seq[String]]("dockerEntrypoint", "Entrypoint arguments passed in exec form")
   val dockerCmd = SettingKey[Seq[String]]("dockerCmd", "Docker CMD. Used together with dockerEntrypoint. Arguments passed in exec form")
