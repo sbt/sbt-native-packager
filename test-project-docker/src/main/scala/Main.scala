1b9bd16f63904e24ceb2f4dca31e677cd3dec300
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2017-05-28 15:26:18 +0200

    Update test-project-docker for #978

--- test-project-docker/src/main/scala/Main.scala
+++ test-project-docker/src/main/scala/Main.scala
@@ -3,9 +3,4 @@ import scala.util._
 
 object Main extends App {
   println("Hello world")
-  
-  val path = Paths get "/data/test01"
-  val result = Try(Files createFile path)
-  
-  println(result)
 }
