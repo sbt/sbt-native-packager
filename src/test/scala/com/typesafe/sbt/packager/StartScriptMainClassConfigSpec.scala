b3c5e904221ef2bab353b719bd9f352b29dcf632
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2017-12-31 16:10:30 +0100

    Validate formatting on PR

--- src/test/scala/com/typesafe/sbt/packager/StartScriptMainClassConfigSpec.scala
+++ src/test/scala/com/typesafe/sbt/packager/StartScriptMainClassConfigSpec.scala
@@ -10,38 +10,44 @@ class StartScriptMainClassConfigSpec extends WordSpec with Matchers {
     "handle a single explicit main class" when {
 
       "main class is not discovered" in {
-        StartScriptMainClassConfig.from(Some("mainClass"), Nil) should === (SingleMain("mainClass"))
+        StartScriptMainClassConfig.from(Some("mainClass"), Nil) should ===(SingleMain("mainClass"))
       }
 
       "main class is discovered" in {
-        StartScriptMainClassConfig.from(Some("mainClass"), Seq("mainClass")) should === (SingleMain("mainClass"))
+        StartScriptMainClassConfig.from(Some("mainClass"), Seq("mainClass")) should ===(SingleMain("mainClass"))
       }
     }
 
     "handle an explicit main class with alternatives" when {
 
       "main class is not discovered" in {
-        StartScriptMainClassConfig.from(Some("mainClass"), Seq("alternate")) should === (ExplicitMainWithAdditional("mainClass", Seq("alternate")))
+        StartScriptMainClassConfig.from(Some("mainClass"), Seq("alternate")) should ===(
+          ExplicitMainWithAdditional("mainClass", Seq("alternate"))
+        )
       }
 
       "main class is discovered" in {
-        StartScriptMainClassConfig.from(Some("mainClass"), Seq("mainClass", "alternate")) should === (ExplicitMainWithAdditional("mainClass", Seq("alternate")))
+        StartScriptMainClassConfig.from(Some("mainClass"), Seq("mainClass", "alternate")) should ===(
+          ExplicitMainWithAdditional("mainClass", Seq("alternate"))
+        )
       }
     }
 
     "handle discovered main classes" when {
 
       "a single main class is discovered" in {
-        StartScriptMainClassConfig.from(None, Seq("mainClass")) should === (SingleMain("mainClass"))
+        StartScriptMainClassConfig.from(None, Seq("mainClass")) should ===(SingleMain("mainClass"))
       }
 
       "multiple main classes are discovered" in {
-        StartScriptMainClassConfig.from(None, Seq("mainClass", "alternate")) should === (MultipleMains(Seq("mainClass", "alternate")))
+        StartScriptMainClassConfig.from(None, Seq("mainClass", "alternate")) should ===(
+          MultipleMains(Seq("mainClass", "alternate"))
+        )
       }
     }
 
     "handle no main classes" in {
-      StartScriptMainClassConfig.from(None, Seq.empty) should === (NoMain)
+      StartScriptMainClassConfig.from(None, Seq.empty) should ===(NoMain)
     }
   }
 }
