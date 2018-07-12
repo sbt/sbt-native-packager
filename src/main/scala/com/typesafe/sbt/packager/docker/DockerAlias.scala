23a73f03a31a7ce38b826c87d211940967a53b12
Taehyun Kim <kgyoo8232@gmail.com>
2018-07-12 16:47:56 +0900

    refactor, fix scripted tests

--- src/main/scala/com/typesafe/sbt/packager/docker/DockerAlias.scala
+++ src/main/scala/com/typesafe/sbt/packager/docker/DockerAlias.scala
@@ -5,18 +5,14 @@ package com.typesafe.sbt.packager.docker
   * It generates a string in the form of {{{[REGISTRY_HOST/][USERNAME/]NAME[:TAG]}}},
   * e.g. ''my-registry.com:1234/my-user/my-service:1.0.0'' or just ''my-service:1.0.0''.
   * @param registryHost Optional hostname of the registry (including port if applicable)
-  * @param username Optional username or other qualifier
   * @param name Name of the image, e.g. the artifact name
-  * @param tag Optional tag for the image, e.g. the version
+  * @param tags Additional tags for the image, e.g. the version
   */
 case class DockerAlias(registryHost: Option[String], username: Option[String], name: String, tags: Seq[String]) {
   protected val distinctTags: Seq[String] = tags.distinct
 
   /** Untagged image alias */
   val untagged: String = registryHost.map(_ + "/").getOrElse("") + username.map(_ + "/").getOrElse("") + name
-
-  /** Tag with version 'latest' */
-  val latest = s"$untagged:latest"
 
   /** Seq of tagged docker image aliases */
   val value: Seq[String] = if (distinctTags.nonEmpty) {
