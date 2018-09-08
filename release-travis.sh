0a1b192a216b67c460e68303d1fad0ac5a9c3481
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2018-09-08 21:18:18 +0200

    Check that tag and master SHA are equal

--- release-travis.sh
+++ release-travis.sh
@@ -15,7 +15,8 @@ ssh-add project/native_packager_deploy_key
 # this leads to an error like this: "ref HEAD is not a symbolic ref"
 # https://github.com/sbt/sbt-release/issues/210#issuecomment-348210828
 # https://stackoverflow.com/questions/6802145/how-to-convert-a-git-shallow-clone-to-a-full-clone/17937889#17937889
-echo "Fixing git setup for $TRAVIS_BRANCH (master)"
+TAG_SHA=$(git rev-parse HEAD)
+echo "Fixing git setup for $TRAVIS_BRANCH ( release branch: $RELEASE_BRANCH )"
 # use ssh for automatic credentials management
 git remote set-url origin git@github.com:${TRAVIS_REPO_SLUG}.git
 # revert the --singleBranch checkout
@@ -23,10 +24,17 @@ git fetch --unshallow
 # make remote branches available
 git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 # fetch the release branch, check it out and follow it
-# TODO we should check that the SHA of $TRAVIS_BRANCH and $RELEASE_BRANCH match
 git fetch origin ${RELEASE_BRANCH}
 git checkout -b "${RELEASE_BRANCH}"
 git branch -u origin/${RELEASE_BRANCH}
+
+MASTER_SHA=$(git rev-parse HEAD)
+if [ $MASTER_SHA != $TAG_SHA ] then
+    echo "You tagged an older commit with SHA $TAG_SHA , but the master is on $MASTER_SHA"
+    echo "Automatic releases are only possible from the current master"
+    exit 1
+fi
+
 
 # configure basic git stuff for commits
 git config user.name "Travis CI"
