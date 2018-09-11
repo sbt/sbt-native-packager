02b495427d476bf498718ff5056489f37e16b457
Travis CI User <travis@example.org>
2018-09-11 14:29:12 +0000

    updated site

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.3.8-M15',
+    VERSION: '1.3.9',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
