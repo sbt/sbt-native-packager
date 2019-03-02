ead5db443620c788b50675fb634b93e0055c0031
Nepomuk Seiler <muuki88@users.noreply.github.com>
2019-03-02 16:48:36 +0100

    FIX #1205 undefined setting makeBashScripts with vanilla DockerPlugin (#1207)
    * FIX #1205 Error "Reference to undefined setting: makeBashScripts from dockerAdditi...
    
    * Scalafmt
    
    * Use linuxDefaultInstallationPath and remove bat files
    
    * Scalafmt

--- src/main/scala/com/typesafe/sbt/packager/docker/DockerPlugin.scala
+++ src/main/scala/com/typesafe/sbt/packager/docker/DockerPlugin.scala
@@ -109,14 +109,15 @@ object DockerPlugin extends AutoPlugin {
     dockerRmiCommand := dockerExecCommand.value ++ Seq("rmi"),
     dockerBuildCommand := dockerExecCommand.value ++ Seq("build") ++ dockerBuildOptions.value ++ Seq("."),
     dockerAdditionalPermissions := {
-      val scripts = makeBashScripts.value
-      val ms = (mappings in Docker).value
-      scripts flatMap {
-        case (script, _) =>
-          ms collect {
-            case (k, v) if k == script => DockerChmodType.UserGroupPlusExecute -> v
-          }
-      }
+      val basePath = (defaultLinuxInstallLocation in Docker).value
+      (mappings in Docker).value
+        .collect {
+          // by default we assume everything in the bin/ folder should be executable that is not a .bat file
+          case (_, path) if path.startsWith(s"$basePath/bin/") && !path.endsWith(".bat") =>
+            DockerChmodType.UserGroupPlusExecute -> path
+          // sh files should also be marked as executable
+          case (_, path) if path.endsWith(".sh") => DockerChmodType.UserGroupPlusExecute -> path
+        }
     },
     dockerCommands := {
       val strategy = dockerPermissionStrategy.value
