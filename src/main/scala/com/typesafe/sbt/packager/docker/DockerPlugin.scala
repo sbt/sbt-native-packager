0258bcf55d8011027c2565fdf66a9fc7afc0e8e1
Boris Capitanu <431091+borice@users.noreply.github.com>
2019-06-12 13:05:18 -0500

    Possible improvement for #1202 (#1235)
    * Possible improvement for #1202
    
    * Fixed code formatting

--- src/main/scala/com/typesafe/sbt/packager/docker/DockerPlugin.scala
+++ src/main/scala/com/typesafe/sbt/packager/docker/DockerPlugin.scala
@@ -369,14 +369,22 @@ object DockerPlugin extends AutoPlugin {
                                 gidOpt: Option[String]): CmdLike =
     Cmd(
       "RUN",
-      (List("id", "-u", daemonUser, "2>", "/dev/null", "||") :::
+      (List("id", "-u", daemonUser, "1>/dev/null", "2>&1", "||") :::
         (gidOpt.fold[List[String]](Nil)(
-        gid => List("((", "getent", "group", gid, "||", "groupadd", "-g", gid, daemonGroup, ")", "&&")
+        gid =>
+          List("((", "getent", "group", gid, "1>/dev/null", "2>&1", "||") :::
+            List("(", "type", "groupadd", "1>/dev/null", "2>&1", "&&") :::
+            List("groupadd", "-g", gid, daemonGroup, "||") :::
+            List("addgroup", "-g", gid, "-S", daemonGroup, "))", "&&")
       )) :::
+        List("(", "type", "useradd", "1>/dev/null", "2>&1", "&&") :::
         List("useradd", "--system", "--create-home") :::
         (uidOpt.fold[List[String]](Nil)(List("--uid", _))) :::
         (gidOpt.fold[List[String]](Nil)(List("--gid", _))) :::
-        List(daemonUser, ")")): _*
+        List(daemonUser, "||") :::
+        List("adduser", "-S") :::
+        (uidOpt.fold[List[String]](Nil)(List("-u", _))) :::
+        List("-G", daemonGroup, daemonUser, "))")): _*
     )
 
   /**
---
+++ src/sbt-test/docker/test-busybox-create-user/build.sbt
@@ -0,0 +1,13 @@
+
+enablePlugins(JavaAppPackaging)
+
+name := "test-busybox-create-user"
+
+version := "0.1.0"
+
+maintainer := "Boris Capitanu <borice@hotmail.com>"
+dockerBaseImage := "anapsix/alpine-java:8"
+daemonUserUid in Docker := Some("2000")
+daemonUser in Docker := "appuser"
+daemonGroupGid in Docker := Some("3000")
+daemonGroup in Docker    := "appgroup"
