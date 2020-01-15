9230ce39d02daa8d208eabf6580232488bd9cd9e
Rodrigo Rodriguez <rodrigo.rn88@gmail.com>
2020-01-15 15:14:52 -0600

    Restore previous methods as deprecated for compatibility issues

--- src/main/scala/com/typesafe/sbt/packager/docker/DockerSupport.scala
+++ src/main/scala/com/typesafe/sbt/packager/docker/DockerSupport.scala
@@ -2,6 +2,14 @@ package com.typesafe.sbt.packager.docker
 
 object DockerSupport {
 
+  @deprecated
+  def chownFlag(version: DockerVersion): Boolean =
+    (version.major == 17 && version.minor >= 9) || version.major > 17
+
+  @deprecated
+  def multiStage(version: DockerVersion): Boolean =
+    (version.major == 17 && version.minor >= 5) || version.major > 17
+
   def chownFlag(version: DockerVersion, apiVersion: DockerApiVersion): Boolean =
     ((version.major == 17 && version.minor >= 9) || version.major > 17) || (apiVersion.major == 1 && apiVersion.minor >= 32)
 
