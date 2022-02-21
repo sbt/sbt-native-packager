f031841b448ce3dde3978b482fe6742d1fe0f947
Kirill Plyashkevich <k.plyashkevich@sportradar.com>
2022-02-21 21:27:59 +0300

    JLinkPlugin improvements (#1499)
    fixes #1446
    
    fix `jdk.unsupported`, sbt style and optimize allocations and additional tests added

--- src/test/scala/com/typesafe/sbt/packager/archetypes/jlink/JlinkSpec.scala
+++ src/test/scala/com/typesafe/sbt/packager/archetypes/jlink/JlinkSpec.scala
@@ -1,5 +1,8 @@
 package com.typesafe.sbt.packager.archetypes.jlink
 
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Paths}
+
 import org.scalatest.{FlatSpec, Matchers}
 import JlinkPlugin.Ignore.byPackagePrefix
 import JlinkPlugin.javaVersionPattern
@@ -28,4 +31,73 @@ class JlinkSpec extends FlatSpec with Matchers {
     // as well handle it.
     """JAVA_VERSION="1.9.0"""" should fullyMatch regex (javaVersionPattern withGroup "9")
   }
+
+  "PackageDependency.parse" should "produce proper dependencies" in {
+    var line =
+      "   akka                                               -> akka                                               akka-actor_2.13-2.6.0.jar"
+    JlinkPlugin.PackageDependency.parse(line) should equal(None)
+
+    line =
+      "   akka                                               -> akka.actor                                         akka-actor_2.13-2.6.0.jar"
+    JlinkPlugin.PackageDependency.parse(line) should equal(
+      Some(
+        JlinkPlugin
+          .PackageDependency("akka", "akka.actor", JlinkPlugin.PackageDependency.JarOrDir("akka-actor_2.13-2.6.0.jar"))
+      )
+    )
+
+    line =
+      "   akka                                               -> java.io                                            java.base"
+    JlinkPlugin.PackageDependency.parse(line) should equal(
+      Some(JlinkPlugin.PackageDependency("akka", "java.io", JlinkPlugin.PackageDependency.Module("java.base")))
+    )
+
+    line =
+      "   akka.actor                                         -> sun.misc                                           JDK internal API (jdk.unsupported)"
+    JlinkPlugin.PackageDependency.parse(line) should equal(
+      Some(
+        JlinkPlugin.PackageDependency("akka.actor", "sun.misc", JlinkPlugin.PackageDependency.Module("jdk.unsupported"))
+      )
+    )
+
+    line =
+      "   akka.http.ccompat                                  -> scala.reflect.api                                  not found"
+    JlinkPlugin.PackageDependency.parse(line) should equal(
+      Some(
+        JlinkPlugin.PackageDependency("akka.http.ccompat", "scala.reflect.api", JlinkPlugin.PackageDependency.NotFound)
+      )
+    )
+
+    line = "   org.test.scala.testproject                    -> org.test.testproject                          classes"
+    JlinkPlugin.PackageDependency.parse(line) should equal(
+      Some(
+        JlinkPlugin.PackageDependency(
+          "org.test.scala.testproject",
+          "org.test.testproject",
+          JlinkPlugin.PackageDependency.Classes
+        )
+      )
+    )
+
+    line = "akka-actor_2.13-2.6.0.jar -> java.base"
+    JlinkPlugin.PackageDependency.parse(line) should equal(None)
+
+    line = "akka-actor_2.13-2.6.0.jar -> not found"
+    JlinkPlugin.PackageDependency.parse(line) should equal(None)
+
+  }
+
+  "JlinkPlugin.parseJdeps" should "generate correct dependencies" in {
+    val input = new String(
+      Files.readAllBytes(Paths.get(this.getClass.getResource("/jdeps_output.txt").toURI)),
+      StandardCharsets.UTF_8
+    );
+    val deps = JlinkPlugin.parseJdeps(input)
+    deps.size should be > 0
+    val m = deps.groupBy(_.source)
+    m(JlinkPlugin.PackageDependency.NotFound).size should equal(65)
+    m(JlinkPlugin.PackageDependency.Classes).size should equal(6)
+    m(JlinkPlugin.PackageDependency.Module("java.base")).size should equal(612)
+  }
+
 }
