sbt/sbt-native-packager

commit 8c5178a7533645f6c600b9b71cd829064790b583

Buildx support (#1527)

src/main/scala/com/typesafe/sbt/packager/docker/DockerPlugin.scala
@@ -83,6 +83,7 @@ object DockerPlugin extends AutoPlugin {
     dockerExposedPorts := Seq(),
     dockerExposedUdpPorts := Seq(),
     dockerExposedVolumes := Seq(),
+    dockerBuildxPlatforms := Seq(),
     dockerLabels := Map(),
     dockerEnvVars := Map(),
     dockerRepository := None,
@@ -273,11 +274,21 @@ object DockerPlugin extends AutoPlugin {
     def publishTask =
       Def.task {
         val _ = publishLocal.value
-        val alias = dockerAliases.value
         val log = streams.value.log
-        val execCommand = dockerExecCommand.value
+        val alias = dockerAliases.value
+        val context = stage.value
+        val multiplatform = dockerBuildxPlatforms.value.nonEmpty
+        val execCommand =
+          if (multiplatform)
+            dockerExecCommand.value ++ Seq(
+              "buildx",
+              "build",
+              s"--platform=${dockerBuildxPlatforms.value.mkString(",")}",
+              "--push"
+            ) ++ dockerBuildOptions.value :+ "."
+          else dockerBuildCommand.value
         alias.foreach { aliasValue =>
-          publishDocker(execCommand, aliasValue.toString, log)
+          publishDocker(context, execCommand, aliasValue.toString, log, multiplatform)
         }
       } tag (Tags.Network, Tags.Publish)
 
@@ -377,7 +388,7 @@ object DockerPlugin extends AutoPlugin {
   }
 
   /**
-    * @param dockerBaseDirectory, the installation directory
+    * @param dockerBaseDirectory , the installation directory
     */
   private final def makeWorkdir(dockerBaseDirectory: String): CmdLike =
     Cmd("WORKDIR", dockerBaseDirectory)
@@ -705,7 +716,7 @@ object DockerPlugin extends AutoPlugin {
       log.info(s"Removed image ${tag}")
   }
 
-  def publishDocker(execCommand: Seq[String], tag: String, log: Logger): Unit = {
+  def publishDocker(context: File, execCommand: Seq[String], tag: String, log: Logger, multiplatform: Boolean): Unit = {
     val loginRequired = new AtomicBoolean(false)
 
     def publishLogger(log: Logger) =
@@ -728,11 +739,11 @@ object DockerPlugin extends AutoPlugin {
         override def buffer[T](f: => T): T = f
       }
 
-    val cmd = execCommand ++ Seq("push", tag)
+    val cmd = if (multiplatform) execCommand else execCommand ++ Seq("push", tag)
 
     log.debug("Executing " + cmd.mkString(" "))
 
-    val ret = sys.process.Process(cmd) ! publishLogger(log)
+    val ret = sys.process.Process(cmd, context) ! publishLogger(log)
 
     if (loginRequired.get)
       sys.error("""No credentials for repository, please run "docker login"""")

src/main/scala/com/typesafe/sbt/packager/docker/Keys.scala
@@ -74,4 +74,6 @@ private[packager] trait DockerKeysEx extends DockerKeys {
       "Setting to true will cause Docker to bundle a tini in the container, to run as the init process, which is recommended for JVM apps. " +
       "Requires Docker API version 1.25+"
   )
+  val dockerBuildxPlatforms =
+    SettingKey[Seq[String]]("dockerBuildxPlatforms", "The docker image platforms for buildx multi-platform build")
 }

test-project-docker/build.sbt
@@ -6,3 +6,5 @@ version := "0.1.0"
 maintainer := "Gary Coady <gary@lyranthe.org>"
 dockerBaseImage := "openjdk:8-jre-alpine"
 dockerUpdateLatest := true
+dockerBuildxPlatforms := Seq("linux/arm64/v8", "linux/amd64")
+dockerUsername := Some("dswiecki")

test-project-docker/project/build.properties
@@ -1,1 +1,1 @@
-sbt.version=1.2.6
+sbt.version=1.5.4

commit ea92791add4b9b71c6b4fd3589aa3d2863ef166a

Fix bug introduced in #1527
See https://github.com/sbt/sbt-native-packager/pull/1527/files#diff-b866751bc91854625b6a5d821ae6a4df02736ceb3581779d81319caee1d9d2cdL278-R289

Error when publishing:
```bash
[error] ERROR: "docker buildx build" requires exactly 1 argument.
[error] See 'docker buildx build --help'.
[error] Usage:  docker buildx build [OPTIONS] PATH | URL | -
[error] Start a build
[error] stack trace is suppressed; run last my-app / Docker / publish for the full output
[error] (my-app / Docker / publish) Nonzero exit value: 1
[error] Total time: 189 s (03:09), completed 17 Jan 2023, 12:33:37 pm
```

src/main/scala/com/typesafe/sbt/packager/docker/DockerPlugin.scala
@@ -286,7 +286,7 @@ object DockerPlugin extends AutoPlugin {
               s"--platform=${dockerBuildxPlatforms.value.mkString(",")}",
               "--push"
             ) ++ dockerBuildOptions.value :+ "."
-          else dockerBuildCommand.value
+          else dockerExecCommand.value
         alias.foreach { aliasValue =>
           publishDocker(context, execCommand, aliasValue.toString, log, multiplatform)
         }