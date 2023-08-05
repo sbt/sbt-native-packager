8db749503029673d610f403e0535b9d05fe5a5b2
Kingston Chan <c.kingston@univapay.com>
2023-08-05 21:35:48 +0900

    Attempt to fix binary compatibility

--- src/main/scala/com/typesafe/sbt/packager/graalvmnativeimage/GraalVMNativeImageKeys.scala
+++ src/main/scala/com/typesafe/sbt/packager/graalvmnativeimage/GraalVMNativeImageKeys.scala
@@ -14,12 +14,12 @@ trait GraalVMNativeImageKeys {
   val graalVMNativeImageGraalVersion = settingKey[Option[String]](
     "Version of GraalVM to build with. Setting this has the effect of generating a container build image to build the native image with this version of GraalVM."
   )
-
-  val graalVMNativeImagePlatformArch = settingKey[Option[String]](
-    "Platform architecture of GraalVM to build with. This only works when building the native image with a container."
-  )
 }
 
 trait GraalVMNativeImageKeysEx extends GraalVMNativeImageKeys {
   val graalVMNativeImageCommand = settingKey[String]("GraalVM native-image executable command")
+
+  val graalVMNativeImagePlatformArch = settingKey[Option[String]](
+    "Platform architecture of GraalVM to build with. This only works when building the native image with a container."
+  )
 }
